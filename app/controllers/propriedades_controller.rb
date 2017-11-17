79312a2ff86e89e3b723d95653a7f58600a7682d
Thiago <thiagopirex@gmail.com>
2017-11-16 23:29:16 -0200

    páginas propriedades

--- app/controllers/propriedades_controller.rb
+++ app/controllers/propriedades_controller.rb
@@ -10,6 +10,7 @@ class PropriedadesController < ApplicationController
   # GET /propriedades/1
   # GET /propriedades/1.json
   def show
+    @sede_ponto = @propriedade.getGeoJsonFromRGeoPoint
   end
 
   # GET /propriedades/new
@@ -19,20 +20,20 @@ class PropriedadesController < ApplicationController
 
   # GET /propriedades/1/edit
   def edit
+    @sede_ponto = @propriedade.getGeoJsonFromRGeoPoint
   end
 
   # POST /propriedades
   # POST /propriedades.json
   def create
     @propriedade = Propriedade.new(propriedade_params)
+    @propriedade.sede = getRGeoFromGeoJson
 
     respond_to do |format|
       if @propriedade.save
-        format.html { redirect_to @propriedade, notice: 'Propriedade was successfully created.' }
-        format.json { render :show, status: :created, location: @propriedade }
+        format.html { redirect_to @propriedade, notice: 'Propriedade criada com sucesso!' }
       else
         format.html { render :new }
-        format.json { render json: @propriedade.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -40,9 +41,12 @@ class PropriedadesController < ApplicationController
   # PATCH/PUT /propriedades/1
   # PATCH/PUT /propriedades/1.json
   def update
+    
     respond_to do |format|
-      if @propriedade.update(propriedade_params)
-        format.html { redirect_to @propriedade, notice: 'Propriedade was successfully updated.' }
+      atributos = propriedade_params.clone
+      atributos[:sede] = getRGeoFromGeoJson
+      if @propriedade.update_attributes(atributos)
+        format.html { redirect_to @propriedade, notice: 'Propriedade atualizada com sucesso!' }
         format.json { render :show, status: :ok, location: @propriedade }
       else
         format.html { render :edit }
@@ -56,7 +60,7 @@ class PropriedadesController < ApplicationController
   def destroy
     @propriedade.destroy
     respond_to do |format|
-      format.html { redirect_to propriedades_url, notice: 'Propriedade was successfully destroyed.' }
+      format.html { redirect_to propriedades_url, notice: 'Propriedade excluída!' }
       format.json { head :no_content }
     end
   end
@@ -65,10 +69,26 @@ class PropriedadesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_propriedade
       @propriedade = Propriedade.find(params[:id])
+      @propriedades = Propriedade.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def propriedade_params
       params.require(:propriedade).permit(:nome, :nirf, :sede)
+    end
+    
+    def getRGeoFromGeoJson
+      #https://github.com/rgeo/rgeo-geojson
+      p = params[:sede_ponto]
+      # geometria = RGeo::GeoJSON.decode(p, json_parser: :json)
+      if p != "valor" && p != ""
+        geo_factory = RGeo::Geographic.spherical_factory(srid: 3857)
+        geometria = RGeo::GeoJSON.decode(p, geo_factory: geo_factory, json_parser: :json)
+        # g = geometria.geometry
+        g = geometria.geometry
+        logger.debug "Tipo da geometria: #{g.geometry_type.type_name}"
+        logger.debug "Geometria: #{g.as_text}"
+        g
+      end
     end
 end
